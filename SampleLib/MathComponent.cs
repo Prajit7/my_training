13703d1c04f9ec12ab2908cd42b4e8634610b4d1
Prajit7 <71118373+Prajit7@users.noreply.github.com>
2023-01-25 18:41:16 +0530

    Add files via upload

---
+++ SampleLib/MathComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleLib
+{
+    public class MathComponent
+    {
+        public double mathComponent(double v1, double v2) => v1 + v2;
+    }
+}
---
+++ SampleLib/Products.cs
@@ -0,0 +1,53 @@
+﻿using SampleLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLib
+{
+    public interface IProductDB
+    {
+        void AddProduct(PRODUCTSDB product);
+        void UpdateProduct(PRODUCTSDB product);
+        void DeleteProduct(int id);
+        List<PRODUCTSDB> GetAllProducts();
+    }
+
+    public static class ProductFactory
+    {
+        public static IProductDB GetComponent() => new ProductNew();
+    }
+    class ProductNew : IProductDB
+    {
+        static ProductDb context = new ProductDb();
+        public void AddProduct(PRODUCTSDB product)
+        {
+            context.PRODUCTSDBs.Add(product);
+            context.SaveChanges();
+        }
+
+        public void DeleteProduct(int id)
+        {
+            var found = context.PRODUCTSDBs.First((p) => p.PRODUCTID == id);
+            context.PRODUCTSDBs.Remove(found);
+            context.SaveChanges();
+
+        }
+
+        public List<PRODUCTSDB> GetAllProducts() => context.PRODUCTSDBs.ToList();
+
+        public void UpdateProduct(PRODUCTSDB product)
+        {
+            var found = context.PRODUCTSDBs.First((p) => p.PRODUCTID == product.PRODUCTID);
+            found.IMAGE = product.IMAGE;
+            found.PRODUCTNAME = product.PRODUCTNAME;
+            found.PRICE = product.PRICE;
+            found.QUANTITY = product.QUANTITY;
+            context.SaveChanges();
+
+        }
+    }
+
+}
